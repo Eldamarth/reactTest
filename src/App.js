a3b8c9e419f03c837d36809ecf985487c0c01f2d
davidagitlen <davidagitlen@gmail.com>
2019-11-15 14:12:16 -0700

    Convert functional component into class component and add cats array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      cats: ["Midi","Hanzo"]
+    }
+  }
+
+  render() {
+    const { cats } = this.state;
+    const catList = cats.map(cat => {
+      return <div id='cat-box'>
+        {cat}
+      </div>
+    });
+    return (
+      <div className="App">
+        {catList}
+      </div>
+    )
+  }
+ 
 }
 
 export default App;
