af4f996eb216d40b2ff6a9f73fea10ad186f5e2e
davidagitlen <davidagitlen@gmail.com>
2019-11-18 16:08:10 -0700

    Implement addCat functionality and include comments to explain steps

--- src/App/App.js
+++ src/App/App.js
@@ -10,9 +10,13 @@ class App extends Component{
     }
   }
 
+  //we will define a function in the parent component app to add a new cat to our list, it will take in the newCat object and reassign state with a built in method on React class components called setState, which is asynchronous. Handy tip: it can accept a second argument of a callback function, which is a very convenient place to put a console log checking the component's state, a console log place directly after it will fire before it has updated 
+
   addCat = newCat => {
     this.setState({ cats : [...this.state.cats, newCat]});
   }
+
+  //we've modified the render here to include the newly imported Form component, and have given it the method addCat as a 'prop,' which is how any information passed down from parent to child is referred to (everything listed in as an attribute in the jsx element will be combined into a props object). We use this pattern of defining a method on the parent and passing it to a child in order to maintain the correct this binding 
 
   render() {
     const { cats } = this.state;
@@ -30,7 +34,7 @@ class App extends Component{
       <div className="App">
         <Form 
           addCat={this.addCat} 
-          />
+        />
         {catList}
       </div>
     )
