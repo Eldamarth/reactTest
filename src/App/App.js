5c5ac34298cdc7b8b7491e22e5561ee00bb0707a
James Kelley <james.a.kelley.mars@gmail.com>
2019-12-24 18:15:07 -0700

    Render catbox component in app
    co-authored by: davidgitlen <davidagitlen@gmail.com>

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from '../Form/Form.js';
+import Catbox from '../Catbox/Catbox.js';
 
 class App extends Component{
   constructor() {
@@ -14,32 +15,24 @@ class App extends Component{
   //we will define a function in the parent component app to add a new cat to our list, it will take in the newCat object and reassign state with a built in method on React class components called setState, which is asynchronous. Handy tip: it can accept a second argument of a callback function, which is a very convenient place to put a console log checking the component's state, a console log place directly after it will fire before it has updated 
 
   addCat = newCat => {
-    this.setState({ cats : [...this.state.cats, newCat]});
+    this.setState({ cats : [...this.state.cats, newCat]}, () => {console.log(this.state)});
   }
 
   removeCat = catToRemove => {
     const { cats } = this.state;
     const filteredCats = cats.filter(cat => cat.name !== catToRemove.name && cat.alignment !== catToRemove.alignment);
-    this.setState({ cats : filteredCats });
+    this.setState({ cats : filteredCats }, () => {console.log(this.state)});
   }
   //we've modified the render here to include the newly imported Form component, and have given it the method addCat as a 'prop,' which is how any information passed down from parent to child is referred to (everything listed as an attribute in the jsx element will be combined into a props object). We use this pattern of defining a method on the parent and passing it to a child in order to maintain the correct this binding 
 
   render() {
     const { cats } = this.state;
-    const catList = cats.map(cat => {
-      return <div id='cat-box'>
-        <p id="name">
-          {cat.name}
-        </p>
-        <p id="alignment">
-          {cat.alignment}
-        </p>
-        <button
-          onClick={() => this.removeCat(cat)}
-        >
-          Remove Cat
-        </button>
-      </div>
+    const catList = cats.map((cat,i) => {
+      return <Catbox 
+        cat={cat} 
+        removeCat={this.removeCat}
+        key={cat.name+i}
+      />
     });
     return (
       <div className="App">
